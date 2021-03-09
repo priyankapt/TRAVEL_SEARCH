157eae5e11f4d296fa42674717a6e27a0aef0c2a
priyankapt <priyankathirukumar@gmail.com>
2021-03-09 09:44:21 +0530

    The files are added

--- src/test/java/com/quinbay/cucumber/steps/hotelsearch.java
+++ src/test/java/com/quinbay/cucumber/steps/hotelsearch.java
@@ -13,16 +13,12 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
-
 import java.lang.invoke.SwitchPoint;
 import java.sql.SQLOutput;
-
-
 public class hotelsearch {
     WebDriver driver;
     WebElement subsearch;
     WebElement outclick;
-
 
     @Given("User is on Blibli Travel page")
     public void userIsOnBlibliTravelPage() {
@@ -37,6 +33,7 @@ public class hotelsearch {
         //WebElement hotel = driver.findElement(By.xpath("//body/div[@id='travel-blibli-app']/div[1]/main[1]/div[5]/main[1]/div[2]/div[1]/div[1]/ul[1]/li[3]/a[1]"));
         WebElement hotel = driver.findElement(By.xpath("//i[@class='bli-hotel-ic']"));
         hotel.click();
+
     }
 
     @And("User enter hotel name as {string}")
@@ -62,15 +59,16 @@ public class hotelsearch {
         //assertThat("Hotel tidak ditemukan", equals(notfound.getText()));
         //WebElement clickfromlist= driver.findElement(By.xpath("(//li[@class='autocomplete__result-item'])[1]/div/div/div"));
         //assertThat(true, is(clickfromlist.isDisplayed()));
-       // boolean notfound=driver.findElement(By.xpath("//div[contains(text(),'Hotel tidak ditemukan')]")).isDisplayed();
-       // assertEquals(true,notfound);
+        // boolean notfound=driver.findElement(By.xpath("//div[contains(text(),'Hotel tidak ditemukan')]")).isDisplayed();
+        // assertEquals(true,notfound);
         System.out.println("....");
         Thread.sleep(5000);
-       driver.close();
+        driver.close();
     }
 
-    @And("User enter country name as {string}")
-    public void userEnterCountryNameAs(String arg0) throws InterruptedException {
+    @And("User enter search name as country name, state name , hotel name")
+    public void userEnterSearchNameAsCountryNameStateNameHotelName() throws InterruptedException {
+        //Thread.sleep(10000);
         Thread.sleep(3000);
         WebElement location = driver.findElement(By.xpath("//body/div[@id='travel-blibli-app']/div[1]/main[1]/div[5]/main[1]/div[2]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/input[1]"));
         //WebElement location = driver.findElement(By.xpath("//input[@class='input']"));
@@ -78,8 +76,8 @@ public class hotelsearch {
         Thread.sleep(2000);
         location.clear();
         Thread.sleep(2000);
-        location.sendKeys(arg0);
-        System.out.println("country name : " + arg0);
+        location.sendKeys("Bali");
+        //System.out.println("country name : " + arg0);
         //WebElement bali= driver.findElement(By.xpath("(//li[@class='autocomplete__result-item'])[1]/div/div/div"));
         //bali.click();
         //Thread.sleep(2000);
@@ -92,15 +90,12 @@ public class hotelsearch {
         WebElement notfound = driver.findElement(By.xpath("//div[contains(text(),'Hotel tidak ditemukan')]"));
         WebElement searchhotelbutton = driver.findElement(By.xpath("//button[contains(text(),'Cari Hotel')]"));
         WebElement outclick = driver.findElement(By.xpath("//body/div[@id='travel-blibli-app']/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]"));
-
         String value = notfound.getText();
-
         if (value.equals("Hotel tidak ditemukan")) {
             System.out.println("hotels not found");
             Thread.sleep(1000);
             int othercallingways = 1;
-            switch (othercallingways)
-            {
+            switch (othercallingways) {
 
                 case 1:
 
@@ -108,12 +103,12 @@ public class hotelsearch {
                     Thread.sleep(3000);
                     recursivefunction();
                     subsearch.sendKeys("Tamil Nadu");
-                    Thread.sleep(3000);
-                    WebElement clickfromlist= driver.findElement(By.xpath("(//li[@class='autocomplete__result-item'])[1]/div/div/div"));
+                    Thread.sleep(4000);
+                    WebElement clickfromlist = driver.findElement(By.xpath("(//li[@class='autocomplete__result-item'])[1]/div/div/div"));
                     clickfromlist.click();
                     //assertThat(true, is(clickfromlist.isDisplayed()));
                     Thread.sleep(3000);
-                    outclick.click();
+                    //outclick.click();
                     Thread.sleep(3000);
                     searchhotelbutton.click();
                     Thread.sleep(3000);
@@ -124,8 +119,6 @@ public class hotelsearch {
                     Thread.sleep(1000);
 
 
-
-
                 case 2:
 
                     Thread.sleep(1000);
@@ -133,8 +126,8 @@ public class hotelsearch {
                     Thread.sleep(1000);
                     recursivefunction();
                     subsearch.sendKeys("Bangalore");
-                    Thread.sleep(3000);
-                    WebElement clickfromlist1= driver.findElement(By.xpath("(//li[@class='autocomplete__result-item'])[1]/div/div/div"));
+                    Thread.sleep(5000);
+                    WebElement clickfromlist1 = driver.findElement(By.xpath("(//li[@class='autocomplete__result-item'])[1]/div/div/div"));
                     clickfromlist1.click();
                     Thread.sleep(3000);
                     searchhotelbutton.click();
@@ -151,19 +144,19 @@ public class hotelsearch {
 
 
                     System.out.println("Search by hotel name");
-                    Thread.sleep(1000);
+                    Thread.sleep(2000);
                     recursivefunction();
                     subsearch.sendKeys("Zest Hotel Airport Jakarta");
-                    Thread.sleep(3000);
-                    WebElement hotelsearch= driver.findElement(By.xpath("(//li[@class='autocomplete__result-item'])[1]/div/div/div"));
+                    Thread.sleep(4000);
+                    WebElement hotelsearch = driver.findElement(By.xpath("(//li[@class='autocomplete__result-item'])[1]/div/div/div"));
                     hotelsearch.click();
-                    Thread.sleep(3000);
+                    Thread.sleep(4000);
                     searchhotelbutton.click();
                     Thread.sleep(3000);
-                    WebElement bookroom= driver.findElement(By.xpath(("//button[@class='see-room-button']")));
-                    bookroom.click();
-                    WebElement add=driver.findElement(By.xpath("//button[@class='room-box-third-section-button']"));
-                    add.click();
+                    //WebElement bookroom = driver.findElement(By.xpath(("//button[@class='see-room-button']")));
+                    //bookroom.click();
+                    //WebElement add = driver.findElement(By.xpath("//button[@class='room-box-third-section-button']"));
+                    //add.click();
                     System.out.println("SEARCHED BY HOTEL NAME");
                     break;
                 case 4:
@@ -176,7 +169,7 @@ public class hotelsearch {
                     Thread.sleep(2000);
                     searchhotelbutton.click();
                     Thread.sleep(30000);
-                    WebElement hotelclick= driver.findElement(By.xpath("//body/div[@id='travel-blibli-app']/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[1]/div[1]/div[2]/span[1]/div[2]/div[1]/a[1]/div[1]/div[2]/span[1]/div[1]"));
+                    WebElement hotelclick = driver.findElement(By.xpath("//body/div[@id='travel-blibli-app']/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[1]/div[1]/div[2]/span[1]/div[2]/div[1]/a[1]/div[1]/div[2]/span[1]/div[1]"));
                     hotelclick.click();
                     Thread.sleep(3000);
                     WebElement bookroom1 = driver.findElement(By.xpath("//body/div[@id='travel-blibli-app']/div[1]/main[1]/div[3]/div[1]/section[1]/div[1]/div[1]/div[2]/div[1]/div[2]/div[4]/div[1]/div[1]/div[1]/div[1]/div[3]/button[1]"));
@@ -184,37 +177,29 @@ public class hotelsearch {
                     Thread.sleep(4000);
                     System.out.println("Room selected");
             }
-        }
-        else {
+        } else {
             System.out.println("Hotels found");
         }
     }
-
-
-
-    public void recursivefunction() throws InterruptedException
-    {
+    public void recursivefunction() throws InterruptedException {
         subsearch = driver.findElement(By.xpath("//input[@class='search-bar-input']"));
         subsearch.click();
         subsearch.clear();
         Thread.sleep(2000);
 
     }
-
-
-    public void hotelnotfound()
-
-    {
+    public void hotelnotfound() {
         WebElement notfound = driver.findElement(By.xpath("//div[contains(text(),'Hotel tidak ditemukan')]"));
         String value = notfound.getText();
         if (value.equals("Hotel tidak ditemukan")) {
             System.out.println("hotels not found for selected filter, try again");
         }
     }
-
-
     public void radiobuttonclick() throws InterruptedException {
         WebElement radio = driver.findElement(By.xpath("//input[@type='checkbox'])[2]"));
         radio.click();
         Thread.sleep(5000);
+    }
+}
+
 
--- src/test/resources/features/HotelSearch.feature
+++ src/test/resources/features/HotelSearch.feature
@@ -4,33 +4,33 @@ Feature: LoginFeature
   Background:
   User navigate to Blibli travel page and click on hotel tab
     Given  User is on Blibli Travel page
-  @Test1
-  Scenario: Verify Hotel search with hotel name
-    When User Clicks on hotel tab
-    And User enter hotel name as "Sanya Yuhuayuan Seaview Hotel"
-    Then Hotel should be found
+#  @Test1
+#  Scenario: Verify Hotel search with hotel name
+#    When User Clicks on hotel tab
+#    And User enter hotel name as "Sanya Yuhuayuan Seaview Hotel"
+#    Then Hotel should be found
 
   @Test2
-  Scenario: Verify Hotel search with country name
+  Scenario: Verify Hotel search with different formats
     When User Clicks on hotel tab
-    And User enter country name as "Bali"
+    And User enter search name as country name, state name , hotel name
     Then Hotel should be found
 
-  @Test3
-  Scenario: Verify Hotel search with state name
-    When User Clicks on hotel tab
-    And User enter state name as "Karnataka"
-    Then Hotel should be found
-
-  @Test4
-  Scenario: Verify Hotel search with city name
-    When User Clicks on hotel tab
-    And User enter city name as "Bangalore"
-    Then Hotel should be found
-
-  @Test5
-  Scenario: Verify Hotel search with point of location name
-    When User Clicks on hotel tab
-    And User enter location name as "Stay Inn, Ba ngalore, Bangalore, Karnataka, India"
-    Then Hotel should be found
+#  @Test3
+#  Scenario: Verify Hotel search with state name
+#    When User Clicks on hotel tab
+#    And User enter state name as "Karnataka"
+#    Then Hotel should be found
+#
+#  @Test4
+#  Scenario: Verify Hotel search with city name
+#    When User Clicks on hotel tab
+#    And User enter city name as "Bangalore"
+#    Then Hotel should be found
+#
+#  @Test5
+#  Scenario: Verify Hotel search with point of location name
+#    When User Clicks on hotel tab
+#    And User enter location name as "Stay Inn, Ba ngalore, Bangalore, Karnataka, India"
+#    Then Hotel should be found
 
